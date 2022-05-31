7ae6e424b5b8a67e5b86268488d58c161c4a76c6
Henry <joof456@gmail.com>
2022-05-31 16:22:31 +0800

    3rd V

--- GitSample/src/Test/main.java
+++ GitSample/src/Test/main.java
@@ -5,6 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("2nd V");
+		//我是組員 修改
+		System.out.println("3rd V");
 	}
 
 }
